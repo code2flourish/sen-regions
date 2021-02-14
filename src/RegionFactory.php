aee09e6aae2b80d50a00ca52a5fbbc35e7e073f6
Adonai Nangui <adonainangui03@gmail.com>
2021-02-14 15:37:42 +0000

    🔥 Code Coverage 🔥

--- src/RegionFactory.php
+++ src/RegionFactory.php
@@ -4,5 +4,21 @@ namespace Natheboy\SenRegions;
 
 class RegionFactory
 {
+    protected $regions = [
+        'Sedhiou',
+        'Dakar',
+        'Kedougou'
+    ];
 
+    public function __construct(array $regions = null)
+    {
+        if ($regions) {
+            $this->regions = $regions;
+        }
+    }
+
+    public function getRandomRegion()
+    {
+        return $this->regions[array_rand($this->regions)];
+    }
 }
--- tests/RegionFactoryTest.php
+++ tests/RegionFactoryTest.php
@@ -2,13 +2,35 @@
 
 namespace Natheboy\SenRegions\Tests;
 
+use Natheboy\SenRegions\RegionFactory;
 use PHPUnit\Framework\TestCase;
 
+/**
+ * Class RegionFactoryTest
+ * @package Natheboy\SenRegions\Tests
+ * @covers \Natheboy\SenRegions\RegionFactory
+ */
 class RegionFactoryTest extends TestCase
 {
     /** @test */
     public function it_returns_a_random_region()
     {
-        $this->assertTrue(true);
+        $regions = new RegionFactory(['Sedhiou']);
+        $region = $regions->getRandomRegion();
+
+        $this->assertSame('Sedhiou', $region);
+    }
+
+    /** @test */
+    public function it_returns_a_predefined_region()
+    {
+        $predefinedRegions = [
+            'Sedhiou',
+            'Dakar',
+            'Kedougou'
+        ];
+        $regions = new RegionFactory();
+        $region = $regions->getRandomRegion();
+        $this->assertContains($region, $predefinedRegions);
     }
 }
