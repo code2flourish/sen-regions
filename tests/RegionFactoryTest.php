87e5ac627b21bf5aee58dcc712bac793c50d8b1f
Adonai Nangui <adonainangui03@gmail.com>
2021-02-14 10:10:17 +0000

    PHPUnit Configuration 🔥

---
+++ tests/RegionFactoryTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Natheboy\SenRegions\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class RegionFactoryTest extends TestCase
+{
+    /** @test */
+    public function it_returns_a_random_region()
+    {
+        $this->assertTrue(true);
+    }
+}
